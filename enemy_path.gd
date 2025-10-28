0ca97b66ff32694e3dee01f8fe0971d6647711fc
Allan Sechrist <sechristallan@gmail.com>
2025-10-28 22:45:51 +0900

    Add difficulty manager and increase spawn rate of enemies as the game progresses

--- enemy_path.gd
+++ enemy_path.gd
@@ -1,8 +1,11 @@
 extends Path3D
 
 @export var enemy_scene: PackedScene
+@export var difficulty_manager: Node
+@onready var enemy_spawn_timer: Timer = $EnemySpawnTimer
 
 func spawn_enemy() -> void:
 	# connected to timeout signal from EnemySpawnTimer
 	var new_enemy = enemy_scene.instantiate()
 	add_child(new_enemy)
+	enemy_spawn_timer.wait_time = difficulty_manager.get_spawn_time()
