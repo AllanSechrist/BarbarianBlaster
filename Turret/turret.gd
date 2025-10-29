2af13f71f4f47c01299f64814c12f7e46dd4e151
Allan Sechrist <sechristallan@gmail.com>
2025-10-29 23:02:54 +0900

    Add assets for base and turret to game

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -3,8 +3,10 @@ extends Node3D
 @export var projectile: PackedScene
 @export var turret_range := 10.0
 
-@onready var turret_top: MeshInstance3D = $TurretBase/TurretTop
+@onready var turret_top: Node3D = $TurretBase/TurretTop
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var cannon: Node3D = $TurretBase/TurretTop/Cannon
+@onready var turret_base: Node3D = $TurretBase
 
 var enemy_path: Path3D
 var target: PathFollow3D
@@ -12,15 +14,15 @@ var target: PathFollow3D
 func _physics_process(delta: float) -> void:
 	target = find_best_target()
 	if target:
-		look_at(target.global_position, Vector3.UP, true)
+		turret_base.look_at(target.global_position, Vector3.UP, true)
 
 func _on_timer_timeout() -> void:
 	if target:
 		var shot = projectile.instantiate()
 		animation_player.play("fire")
 		add_child(shot)
-		shot.global_position = turret_top.global_position
-		shot.direction = global_transform.basis.z
+		shot.global_position = cannon.global_position
+		shot.direction = turret_base.global_transform.basis.z
 
 func find_best_target() -> PathFollow3D:
 	var best_target = null
